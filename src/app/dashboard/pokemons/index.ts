0368c5c3bd8b6c9d57847bec8c83e68a2a8bc269
warmab13 <alonsosalcido13@gmail.com>
2024-02-20 13:10:37 -0600

    pokemon card component and create card for pokemon

--- src/app/dashboard/pokemons/index.ts
+++ src/app/dashboard/pokemons/index.ts
@@ -1,2 +1,5 @@
+export { PokemonGrid } from './components/PokemonGrid';
+
+
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,5 +1,6 @@
 import { PokemonsResponse, SimplePokemon } from ".";
 import Image from 'next/image';
+import { PokemonGrid } from './components/PokemonGrid';
 
 const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon> => {
     const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then( res => res.json() );
@@ -18,18 +19,8 @@ export default async function PokemonsPage() {
 
     return (
         <div className="flex flex-col">
-            <div className="flex flex-wrap gap-10 items-center justify-center">
-                {
-                    pokemons.map( pokemon => (
-                        <Image 
-                            key = { pokemon.id }
-                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                            width={100}
-                            height={100}
-                            alt={pokemon.name}/>
-                    ))
-                }
-            </div>
+            <span className="text-5xl my-2">Listado de Pokemons <small>estatico</small></span>
+            <PokemonGrid pokemons={pokemons}/>
         </div>
     );
 }

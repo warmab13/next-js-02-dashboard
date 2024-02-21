768ded6fac8046d7272e5815547673f428bdd463
warmab13 <alonsosalcido13@gmail.com>
2024-02-21 11:00:30 -0600

    solving error on get pokemons using interface on src/app/dashboard/pokemons/page/getPokemons.tsx

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,13 +1,11 @@
-import { PokemonsResponse, SimplePokemon } from "../../../pokemons";
-import Image from 'next/image';
-import { PokemonGrid } from '../../../pokemons/components/PokemonGrid';
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "../../../pokemons";
 
-const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon> => {
+const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
     const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).then( res => res.json() );
 
     const pokemons = data.results.map( pokemon => ({
         id: pokemon.url.split('/').at(-2)!,
-        name: pokemon.name,
+        name: pokemon.name
     }))
 
     //throw new Error('Esto es un error que no deberia de suceder');

1695544cafc56bd20a1eec4664f83004709f9db3
warmab13 <alonsosalcido13@gmail.com>
2024-03-14 08:52:48 -0600

    getting counter data from server using restfull api

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -7,18 +7,34 @@ interface Props {
   value?: number;
 }
 
+export interface CounterResponse {
+  method: string,
+  count: number
+}
+
+const getApiCounter = async():Promise<CounterResponse> => {
+  const data = await fetch('/api/counter').then(res => res.json());
+  console.log({data});
+  return data;
+}
+
 export const CartCounter = ({ value = 0 }: Props) => {
   const count = useAppSelector( state => state.counter.count );
   const dispatch = useAppDispatch();
   //const [count, setCount] = useState(value)
 
+  // useEffect(()=>{
+  //   dispatch( initCounterState(value) )
+  // }, [dispatch, value])
+
   useEffect(()=>{
-    dispatch( initCounterState(value) )
-  }, [dispatch, value])
+    getApiCounter()
+    .then(({ count }) => dispatch( initCounterState(count) ));
+  }, [dispatch])
   
   return (
     <>
-      <span className="text-9xl">{count}</span>
+      <span className="text-9xl">{ count }</span>
       <div className="flex">
         <button onClick={() => dispatch( addOne() )} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
